715f257480e640e64c4df0de6d4f0c43984bf41e
hansol <jjjang6217@gmail.com>
2017-04-12 02:06:21 +0900

    Data Integrity - MD5 Test - can be integrity the file

--- client/datatest/md5.cpp
+++ client/datatest/md5.cpp
@@ -1,57 +1,3 @@
-/*
-Copyright (C) 1999, 2000, 2002 Aladdin Enterprises.  All rights reserved.
-
-This software is provided 'as-is', without any express or implied
-warranty.  In no event will the authors be held liable for any damages
-arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented; you must not
-claim that you wrote the original software. If you use this software
-in a product, an acknowledgment in the product documentation would be
-appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be
-misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
-
-L. Peter Deutsch
-ghost@aladdin.com
-
-*/
-/* $Id: md5.c,v 1.6 2002/04/13 19:20:28 lpd Exp $ */
-/*
-Independent implementation of MD5 (RFC 1321).
-
-This code implements the MD5 Algorithm defined in RFC 1321, whose
-text is available at
-http://www.ietf.org/rfc/rfc1321.txt
-The code is derived from the text of the RFC, including the test suite
-(section A.5) but excluding the rest of Appendix A.  It does not include
-any code or documentation that is identified in the RFC as being
-copyrighted.
-
-The original and principal author of md5.c is L. Peter Deutsch
-<ghost@aladdin.com>.  Other authors are noted in the change history
-that follows (in reverse chronological order):
-
-2002-04-13 lpd Clarified derivation from RFC 1321; now handles byte order
-either statically or dynamically; added missing #include <string.h>
-in library.
-2002-03-11 lpd Corrected argument list for main(), and added int return
-type, in test program and T value program.
-2002-02-21 lpd Added missing #include <stdio.h> in test program.
-2000-07-03 lpd Patched to eliminate warnings about "constant is
-unsigned in ANSI C, signed in traditional"; made test program
-self-checking.
-1999-11-04 lpd Edited comments slightly for automatic TOC extraction.
-1999-10-18 lpd Fixed typo in header comment (ansi2knr rather than md5).
-1999-05-03 lpd Original version.
-*/
-
-
 #ifdef HAVE_CONFIG_H 
 #include <config.h> 
 #endif 
@@ -417,6 +363,20 @@ string md5(const string strMd5)
 
 int main(int argc, char *argv[])
 {
-	cout << md5("1234567") << endl;
+	int n=0;
+	char buf[1024] = {};
+	FILE* fp = fopen("C:/Users/HANSOL/sw-project/client/datatest/test.txt", "r");
+
+	if (fp == NULL) {
+		printf("can't read file\n");
+		exit(1);
+	}
+
+	while ((n = fread(buf, 1, 1024, fp)) > 0)
+	{
+		buf[n] = 0;
+		cout << md5(buf) << '\n';
+	}
+
 	return EXIT_SUCCESS;
 }
