7293218be1c1978dee16f8121b0d33cd85facb7d
Joona Yoon <joonas.yoon@gmail.com>
2017-04-21 06:32:40 +0900

    UDP를 TCP로 변경

--- lib/socketLayer.h
+++ lib/socketLayer.h
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <winsock2.h>
+#include <WS2tcpip.h> // TCP/IP
 #include <string>
 #include <vector>
 using namespace std;
@@ -26,29 +27,11 @@ public:
 		WSACleanup();
 	}
 
-	SOCKET Create(SOCKET *source = NULL){
-		if( source == NULL ) source = new SOCKET;
-		*source = socket(AF_INET, SOCK_DGRAM, 0);
-		return *source;
-	}
+	SOCKET Create(SOCKET *source = NULL){ }
 
-	sockaddr_in MakeAddress(string destIP, int port){
-		sockaddr_in addr;
-		memset((void *)&addr, 0x00, sizeof(addr));
-		addr.sin_family = AF_INET;
-		addr.sin_addr.S_un.S_addr = inet_addr(destIP.data());
-		addr.sin_port = htons(port);
-		return addr;
-	}
+	sockaddr_in MakeAddress(string destIP, int port){ }
 
-	sockaddr_in MakeAddress(int port){
-		sockaddr_in addr;
-		memset((void *)&addr, 0x00, sizeof(addr));
-		addr.sin_family = AF_INET; 
-		addr.sin_addr.s_addr = INADDR_ANY; // 모든 사용 가능한 주소로부터 기다리겠다는 의미
-		addr.sin_port = htons( port ); 
-		return addr;
-	}
+	sockaddr_in MakeAddress(int port){ }
 
 	int Connect(SOCKET &socket, sockaddr_in &address){
 		return connect(socket, (struct sockaddr*)&address, sizeof(address));
@@ -100,4 +83,79 @@ private:
 
   SOCKADDR_IN serverAddress;
 	SOCKADDR_IN clientAddress;
+};
+
+class SocketLayerUDP : public SocketLayer {
+public:
+	SOCKET Create(SOCKET *source = NULL){
+		if(source == NULL) source = new SOCKET;
+		*source = socket(AF_INET, SOCK_DGRAM, 0);
+		return *source;
+	}
+
+	sockaddr_in MakeAddress(string destIP, int port){
+		sockaddr_in addr;
+		memset((void *)&addr, 0x00, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_addr.S_un.S_addr = inet_addr(destIP.data());
+		addr.sin_port = htons(port);
+		return addr;
+	}
+
+	sockaddr_in MakeAddress(int port){
+		sockaddr_in addr;
+		memset((void *)&addr, 0x00, sizeof(addr));
+		addr.sin_family = AF_INET; 
+		addr.sin_addr.s_addr = INADDR_ANY; // 모든 사용 가능한 주소로부터 기다리겠다는 의미
+		addr.sin_port = htons( port ); 
+		return addr;
+	}
+};
+
+class SocketLayerTCP : public SocketLayer {
+public:
+	SOCKET Create(SOCKET *source = NULL){
+		if(source == NULL) source = new SOCKET;
+		*source = socket(AF_INET, SOCK_STREAM, 0);
+		return *source;
+	}
+
+	sockaddr_in MakeAddress(string destIP, int port){
+		sockaddr_in addr;
+		memset((void *)&addr, 0x00, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(port);
+		inet_pton(AF_INET, destIP.data(), &addr.sin_addr.s_addr);
+		return addr;
+	}
+
+	sockaddr_in MakeAddress(int port){
+		sockaddr_in addr;
+		memset((void *)&addr, 0x00, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);  // 현재 PC IP주소 사용
+		addr.sin_port = htons(port);        // 포트번호
+		return addr;
+	}
+
+	bool Listen(SOCKET& serverSocket, int backlog = 1){
+		return listen(serverSocket, backlog) != SOCKET_ERROR;
+	}
+
+	SOCKET Accept(SOCKET& serverSocket, sockaddr_in &socketInfo){
+		int addrLength = sizeof(socketInfo);
+		return accept(serverSocket, (struct sockaddr*)&socketInfo, &addrLength);
+	}
+
+	SOCKET StartServer(SOCKET& serverSocket, sockaddr_in& clientSocketInfo){
+		SOCKET cli = Accept(serverSocket, clientSocketInfo);
+		if(cli == INVALID_SOCKET){
+			perror("Accept Error");
+			exit(EXIT_FAILURE);
+		} else {
+			printf("%s Connection Complete!\n", inet_ntoa(clientSocketInfo.sin_addr));
+			printf("Start ...\n");
+		}
+		return cli;
+	}
 };
